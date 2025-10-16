3988aab898d85cf4b35aed8ce49cff88e0afc266
bibzeux <leonesbibiano@gmail.com>
2025-10-17 05:07:55 +0800

    Change the form icon to Account with dropdown button (login,register)

--- misc/headernavfooter.php
+++ misc/headernavfooter.php
@@ -5,35 +5,43 @@ function navbarcall()
     $html = <<<HTML
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container">
-            <a class="navbar-brand" href="index.php">WEyewear</a>
-            <button class="navbar-toggler" type="button">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+                <a class="navbar-brand" href="index.php">WEyewear</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-            <form class="search-nav d-flex" action="search.php" method="GET">
-                <input 
-                    class="form-control me-2" 
-                    type="search" 
-                    name="search_query" 
-                    placeholder="Search products..." 
-                    aria-label="Search"
-                    required
-                >
-                <button class="btn btn-outline-light" type="submit">Search</button>
-            </form>
+                <form class="search-nav d-flex ms-3 me-3" action="search.php" method="GET">
+                    <input 
+                        class="form-control me-2" 
+                        type="search" 
+                        name="search_query" 
+                        placeholder="Search products..." 
+                        aria-label="Search"
+                        required
+                    >
+                    <button class="btn btn-outline-light" type="submit">Search</button>
+                </form>
 
+                <div class="collapse navbar-collapse" id="mainNavbar">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="products.php">Products</a></li>
+                        <li class="nav-item"><a class="nav-link" href="about.php">About Us</a></li>
+                        <li class="nav-item"><a class="nav-link" href="contact-us.php">Contact Us</a></li>
+                        <li class="nav-item"><a class="nav-link" href="cart.php">🛒 Cart</a></li>
 
-
-            <div class="collapse navbar-collapse">
-                <ul class="navbar-nav ms-auto">
-                <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="products.php">Products</a></li>
-                <li class="nav-item"><a class="nav-link" href="about.php">About Us</a></li>
-                <li class="nav-item"><a class="nav-link" href="contact-us.php">Contact Us</a></li>
-                <li class="nav-item"><a class="nav-link" href="cart.php">🛒 Cart</a></li>
-                <li class ="nav-item"><a class="nav-link" href="#" id="loginButton">👤</a></li>
-                </ul>
-            </div>
+                        <!-- Account Dropdown -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="accountDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="fas fa-user-circle"></i> Account
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="accountDropdown">
+                                <li><a class="dropdown-item" href="#" id="loginButton">Login</a></li>
+                                <li><a class="dropdown-item" href="#" id="registerButton">Register</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     HTML;

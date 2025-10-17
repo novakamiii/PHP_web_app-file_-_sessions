4c119d03e1faba7c6ace7769a4c70eedabbb00ee
nova <pauloneil3334@gmail.com>
2025-10-17 15:37:58 +0800

    Updated Session for login log-out

--- misc/headernavfooter.php
+++ misc/headernavfooter.php
@@ -6,6 +6,24 @@ if (session_status() === PHP_SESSION_NONE) {
 
 function navbarcall()
 {
+    //Checks if user is logged in:
+    if (!isset($_SESSION['user_id']))
+    {
+        $logSignHTML = <<<HTML
+            <li><a class="dropdown-item" href="#" id="loginButton">Login</a></li>
+            <li><a class="dropdown-item" href="#" id="registerButton">Register</a></li>
+        HTML;
+    }
+    else
+    {
+        $name = $_SESSION['user_name'];
+        $logSignHTML = <<<HTML
+            <li><p class=" dropdown-item fw-bold">Hi, $name!</p></li>
+            <hr>
+            <li><a class="dropdown-item" href="#" id="logoutButton">Logout</a></li>
+        HTML;
+    }
+
     $html = <<<HTML
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container">
@@ -37,11 +55,10 @@ function navbarcall()
                         <!-- Account Dropdown -->
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="accountDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <i class="fas fa-user-circle"></i> Account
+                                <i class="fas fa-user-circle"></i>
                             </a>
                             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="accountDropdown">
-                                <li><a class="dropdown-item" href="#" id="loginButton">Login</a></li>
-                                <li><a class="dropdown-item" href="#" id="registerButton">Register</a></li>
+                                $logSignHTML
                             </ul>
                         </li>
                     </ul>
@@ -57,9 +74,9 @@ function navbarcall()
 function footer()
 {
     $html = <<<HTML
-        <footer class="mt-auto bg-black text-center py-2 text-secondary small">
-            © 2025 Weyewear | Designed for demo purposes
-        </footer>
+            <footer class="mt-auto bg-black text-center py-2 text-secondary small">
+                © 2025 Weyewear | Designed for demo purposes
+            </footer>
     HTML;
 
     echo $html;
